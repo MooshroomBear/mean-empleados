9ef61efb0fa4961cedbf6c2320c772f393d17a86
MooshroomBear <alexisdm.ti19@utsjr.edu.mx>
2022-06-03 15:28:30 -0500

    first version

---
+++ backEnd/models/Empleado.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongooe')
+const Schema =mongoose.Schema
+
+let Empleado = new Schema({
+    nombre:{
+        type:String
+    },
+    departamento:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    telefono:{
+        type:Number
+    }
+},{
+    collection:'empleadosds02'
+
+})
+module.exports=mongoose.model('Empleado',Empleado)
---
+++ backEnd/server.js
@@ -0,0 +1,47 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const cors=require('cors')
+const bodyParser=require('body-parser')
+const path=require('path')
+//conection con la base de datos
+mongoose
+    .connect('mongodb://127.0.0.1:27017/empleadosds02')
+    .then((x)=>{
+    console.log(`Conectado exitosamente a mongo a la base de datos:"${x.connections[0].name}"`)
+    })
+    .catch((err)=>{
+        console.error('Error al conectarse con Mongo',err.reason)
+    })
+//configuracion del servidor web
+const empleadoRuta=require('../backEnd/routes/empleado.route')
+const app=express()
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlenocded({
+        extended:false,
+    })
+)
+
+app.use(cors())
+app.use(express.static(path.join(__dirname,'dist/mean-empleados')))
+app.use('/',express.static(path.join(__dirname,'dist/mean-empleados')))
+app.use('/api',empleadoRuta)
+
+//habilitar puerto
+const port=process.env.PORT||4000
+const server=app.listen(port,()=>{
+    console.log('Conectado al puerto '+port)
+})
+
+// manejador de error 404
+app.use((req,res,next)=>{
+    next(createError(404))
+})
+
+//manejador de errores
+app.use(function(err,req,res,next){
+    console.error(err.message)
+    if(!err.statusCode)err.statusCode=500
+    res.status(err.statusCode).send(err.message)
+})
